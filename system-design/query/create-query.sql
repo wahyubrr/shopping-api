fe982b4475b03ed1d97d0c440bfcf4280a86d5e4
Wahyu Berlianto <wahyubrlianto@gmail.com>
2022-06-03 03:08:59 +0700

    sql last scratch query

--- system-design/query/create-query.sql
+++ system-design/query/create-query.sql
@@ -1,5 +1,6 @@
-CREATE DATABASE shoppingapi;
-USE shoppingapi;
+CREATE DATABASE ShoppingDB;
+USE ShoppingDB;
+
 CREATE TABLE Customers (
 	Customer_Id int NOT NULL AUTO_INCREMENT,
     Name varchar(100) NOT NULL,
@@ -15,24 +16,14 @@ CREATE TABLE Customers (
 ALTER TABLE Customers
 MODIFY COLUMN Password_Hash char(64) NOT NULL;
 CREATE TABLE Customer_Cart (
-	Cart_Id int NOT NULL AUTO_INCREMENT,
     Customer_Id int NOT NULL,
     Total float NOT NULL,
-    Completed bool NOT NULL,
-    PRIMARY KEY (Cart_Id),
+    Weight_Gram int NOT NULL,
+    PRIMARY KEY (Customer_Id),
     FOREIGN KEY (Customer_Id) REFERENCES Customers(Customer_Id)
 );
 ALTER TABLE Customer_Cart
-ADD COLUMN Completed bool NOT NULL;
-CREATE TABLE Cart_Item (
-	Cart_Item_Id int NOT NULL AUTO_INCREMENT,
-    Cart_Id int NOT NULL,
-    Product_Id int NOT NULL,
-    Quantity int NOT NULL,
-    PRIMARY KEY (Cart_Item_Id),
-    FOREIGN KEY (Cart_Id) REFERENCES Customer_Cart(Cart_Id),
-    FOREIGN KEY (Product_Id) REFERENCES Products(Product_Id)
-);
+ADD COLUMN Weight_Gram int NOT NULL;
 CREATE TABLE Product_Category (
 	Category_Id varchar(50) NOT NULL,
     Category_Title varchar(50) NOT NULL,
@@ -49,21 +40,29 @@ CREATE TABLE Products (
     PRIMARY KEY (Product_Id),
     FOREIGN KEY (Category_Id) REFERENCES Product_Category(Category_Id)
 );
+CREATE TABLE Cart_Item (
+	Cart_Item_Id int NOT NULL AUTO_INCREMENT,
+    Customer_Id int NOT NULL,
+    Product_Id int NOT NULL,
+    Quantity int NOT NULL,
+    PRIMARY KEY (Cart_Item_Id),
+    FOREIGN KEY (Customer_Id) REFERENCES Customer_Cart(Customer_Id),
+    FOREIGN KEY (Product_Id) REFERENCES Products(Product_Id)
+);
 CREATE TABLE Orders (
 	Order_Id int NOT NULL AUTO_INCREMENT,
     Customer_Id int NOT NULL,
     Total float NOT NULL,
-    Weight int NOT NULL,
-    Street varchar(50) NOT NULL,
-    City varchar(30) NOT NULL,
-    Zip varchar(10) NOT NULL,
-    Phone varchar(15) NOT NULL,
-    Order_date date NOT NULL,
+    Weight_Gram int NOT NULL,
+    Order_Date timestamp NOT NULL,
     Logistic varchar(10) NOT NULL,
     Tracking_Number varchar(20) NOT NULL,
+    Payment_Completed bool NOT NULL,
     PRIMARY KEY (Order_Id),
     FOREIGN KEY (Customer_Id) REFERENCES Customers(Customer_Id)
 );
+ALTER TABLE Orders ADD Payment_Completed bool NOT NULL;
+ALTER TABLE Orders MODIFY COLUMN Order_Date timestamp NOT NULL; 
 CREATE TABLE Order_Item (
 	Order_Item_Id int NOT NULL AUTO_INCREMENT,
     Order_Id int NOT NULL,
--- system-design/query/test-query.sql
+++ system-design/query/test-query.sql
@@ -1,12 +1,134 @@
+USE ShoppingDB;
 SELECT * FROM Customers;
 SELECT Email, Password_Hash FROM Customers WHERE Email="salsa@gmail.com" AND Password_Hash="takganti";
 SELECT * FROM Customer_Cart;
+UPDATE Customer_Cart SET Weight_Gram=210 WHERE Customer_Id=1;
 SELECT * FROM Cart_Item;
 SELECT * FROM Product_Category;
 SELECT * FROM Products;
 SELECT * FROM Products WHERE Category_Id="chain";
 SELECT * FROM Orders;
 SELECT * FROM Order_Item;
+
+UPDATE Customer_Cart SET Weight_Gram=(SELECT CC.Weight_Gram - (P.Weight_Gram * CI.Quantity)
+FROM (SELECT Weight_Gram, Customer_Id FROM Customer_Cart) AS CC, (SELECT Weight_Gram, Product_Id FROM Products) AS P,
+	(SELECT Quantity, Product_Id FROM Cart_Item) AS CI
+WHERE CC.Customer_Id=1 AND P.Product_Id=1 AND CI.Product_Id=1)
+WHERE Customer_Cart.Customer_Id=1;
+
+SELECT CC.Weight_Gram - (P.Weight_Gram * CI.Quantity)
+FROM (SELECT Weight_Gram, Customer_Id FROM Customer_Cart) AS CC, (SELECT Weight_Gram, Product_Id FROM Products) AS P,
+	(SELECT Quantity, Product_Id FROM Cart_Item) AS CI
+WHERE CC.Customer_Id=1 AND P.Product_Id=1 AND CI.Product_Id=1;
+
+UPDATE Customer_Cart SET Weight_Gram=(SELECT CC.Weight_Gram + (P.Weight_Gram * 2)
+FROM (SELECT Weight_Gram, Customer_Id FROM Customer_Cart) AS CC, (SELECT Product_Id, Weight_Gram FROM Products) AS P
+WHERE P.Product_Id=4 AND CC.Customer_Id=1)
+WHERE Customer_Cart.Customer_Id=1;
+
+SELECT CC.Weight_Gram + (P.Weight_Gram * 2)
+FROM (SELECT Weight_Gram, Customer_Id FROM Customer_Cart) AS CC, (SELECT Product_Id, Weight_Gram FROM Products) AS P
+WHERE P.Product_Id=4 AND CC.Customer_Id=1;
+
+UPDATE Customer_Cart SET Weight_Gram=100 WHERE Customer_Id=1;
+
+DELETE FROM Order_Item WHERE Order_Id=6;
+
+INSERT INTO Order_Item (Order_Id, Product_Id, Quantity)
+SELECT
+	(SELECT Order_Id FROM Orders WHERE Order_Date=(
+	SELECT MAX(Order_Date) FROM Orders
+    )
+),
+Cart_Item.Product_Id, Cart_Item.Quantity FROM Cart_Item;
+
+SELECT Order_Id FROM Orders WHERE Order_Date=(
+	SELECT MAX(Order_Date) FROM Orders
+);
+
+SELECT MAX(Order_Date) AS DATES FROM Orders WHERE Customer_Id=1;
+
+-- MODEL CHECKOUT
+-- parameters: Customer_Id, Logistic, Tracking_Number(random from controller, mockup only)
+-- needed column: Customer_Id, Customer_Cart.Total, Weight-tbd, Order_Date NOW(), Logistic, Tracking_Number
+INSERT INTO Orders (Customer_Id, Total, Weight_Gram, Order_Date, Logistic, Tracking_Number)
+	SELECT Customers.Customer_Id, Customer_Cart.Total, 1250 AS Weight_Gram,
+	NOW() AS Order_Date, "JNE" AS Logistic, "jne12345" AS Tracking_Number
+    FROM Customers, Customer_Cart WHERE Customers.Customer_Id=1 AND Customer_Cart.Customer_Id=1;
+
+DELETE FROM Cart_Item WHERE Customer_Id=1;
+DELETE FROM Customer_Cart WHERE Customer_Id=1;
+
+DELETE FROM Cart_Item WHERE CUstomer_Id=1;
+UPDATE Customer_Cart SET Total=0 WHERE Customer_Id=1;
+
+(SELECT CC.Total - (P.Price * CI.Quantity) FROM (SELECT Total, Customer_Id FROM Customer_Cart) AS CC,
+(SELECT Price, Product_Id FROM Products) AS P, (SELECT Quantity, Product_Id FROM Cart_Item) AS CI
+WHERE CC.Customer_Id=1 AND P.Product_Id=6 AND CI.Product_Id=6);
+
+UPDATE Customer_Cart SET Total=(SELECT CC.Total - (P.Price * CI.Quantity) FROM (SELECT Total, Customer_Id FROM Customer_Cart) AS CC,
+(SELECT Price, Product_Id FROM Products) AS P, (SELECT Quantity, Product_Id FROM Cart_Item) AS CI
+WHERE CC.Customer_Id=1 AND P.Product_Id=6 AND CI.Product_Id=6)
+WHERE Customer_Cart.Customer_Id=1;
+
+
+SELECT Products.Product_Id, Products.Category_Id, Products.Title, Products.Quantity, Products.Price, Products.Weight_Gram,
+	Products.Description, Cart_Item.Quantity AS Cart_Quantity, Customer_Cart.Total AS Total
+FROM ((Products
+INNER JOIN Cart_Item ON Products.Product_Id=Cart_Item.Product_Id)
+INNER JOIN Customer_Cart ON Cart_Item.Cart_Id=Customer_Cart.Cart_Id)
+WHERE Cart_Item.Cart_Id=1;
+
+-- Diketahui: customer_id dan product_id
+-- objective: Customer_Cart.Total = Customer_Cart.Total - (Products.Price * Cart_Item.Quantity)
+UPDATE Customer_Cart SET Total=(
+	SELECT CC.Total - (P.Price * CI.Quantity) FROM (SELECT Total, Customer_Id FROM Customer_Cart) AS CC, (SELECT Price, Product_Id FROM Products) AS P, (SELECT Quantity, Product_Id FROM Cart_Item) AS CI
+	WHERE CC.Customer_Id=1 AND P.Product_Id=2 AND CI.Product_Id=2
+) WHERE Customer_Cart.Cart_Id=(
+	SELECT CC.Cart_Id FROM (SELECT Cart_Id, Customer_Id FROM Customer_Cart) AS CC WHERE CC.Customer_Id=1
+);
+
+SELECT CC.Total - (P.Price * CI.Quantity) FROM (SELECT Total, Customer_Id FROM Customer_Cart) AS CC, (SELECT Price, Product_Id FROM Products) AS P, (SELECT Quantity, Product_Id FROM Cart_Item) AS CI
+WHERE CC.Customer_Id=1 AND P.Product_Id=1 AND CI.Product_Id=1 ;
+
+UPDATE Customer_Cart SET Total=0 WHERE Customer_Id=1;
+
+SELECT Customer_Cart.Total + Products.Price FROM Customer_Cart, Products
+WHERE Products.Product_Id=2;
+
+-- Update the total cart
+-- parameters: product_id, cart_id
+UPDATE Customer_Cart SET Total=(
+	SELECT CC.Total + (P.Price * 2) FROM (SELECT Total, Cart_Id FROM Customer_Cart) AS CC, (SELECT Product_Id, Price FROM Products) AS P
+	WHERE P.Product_Id=8 AND CC.Cart_Id=2
+)
+WHERE Customer_Cart.Cart_Id=2;
+
+-- CUSTOMER - REGISTER scratch code
+-- parameters: email from Customers
+INSERT INTO Customers (Name, Email, Street, City, Zip, Phone, Password_Hash, Date_of_Birth)
+SELECT
+	'Ash',
+    'ash@gmail.com',
+    'Jl. Kembar', 
+    'Malang',
+    '60333',
+    '084123456',
+    'takganti',
+    '1996-02-01'
+WHERE (
+	SELECT COUNT(Email)FROM Customers WHERE Email='ash@gmail.com'
+)=0;
+
+SELECT COUNT(Email)FROM Customers WHERE Email='wahyubrlianto@gmail.com';
+
+-- DELETECART scratch code
+-- parameters: customer_id and product_id
+DELETE FROM Cart_Item
+WHERE Product_Id=6 AND Cart_Id=(
+	SELECT Cart_Id FROM Customer_Cart
+    WHERE Customer_Id=1
+);
 
 INSERT INTO Product_Category (
 	Category_Id, Category_Title
